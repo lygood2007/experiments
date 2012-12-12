58264782158ee43a271fc8dd9683eee45edc928e
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2012-12-12 00:37:29 -0200

    Expensive non-blocking code

--- node.js/requestHandlers.js
+++ node.js/requestHandlers.js
@@ -5,11 +5,14 @@ function start (response) {
 
 	var content = "empty";
 
-	exec("ls -lah", function (error, stdout, stderr) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout);
-		response.end();
-	});
+	exec("find /", 
+		{timeout: 10000, maxBuffer: 20000 * 1024},
+		function (error, stdout, stderr) {
+			response.writeHead(200, {"Content-Type": "text/plain"});
+			response.write(stdout);
+			response.end();
+		}
+	);
 }
 
 function upload (response) {
