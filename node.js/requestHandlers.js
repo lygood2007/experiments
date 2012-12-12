3885fe3e13514ce88010e8a62c6a997b1fa4583b
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2012-12-12 00:18:58 -0200

    Blocking code.

--- node.js/requestHandlers.js
+++ node.js/requestHandlers.js
@@ -1,5 +1,12 @@
 function start () {
 	console.log("Request handler 'start' was called.");
+
+	function sleep (millis) {
+		var start = new Date().getTime();
+		while (new Date().getTime() < start + millis);
+	}
+
+	sleep(10000);
 	return "Hello Start";
 }
 
