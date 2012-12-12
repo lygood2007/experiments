9e83ca70403addc999779a3a4c4df49163443219
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2012-12-12 00:34:37 -0200

    Non-blocking code

--- node.js/requestHandlers.js
+++ node.js/requestHandlers.js
@@ -1,18 +1,22 @@
-function start () {
+var exec = require("child_process").exec;
+
+function start (response) {
 	console.log("Request handler 'start' was called.");
 
-	function sleep (millis) {
-		var start = new Date().getTime();
-		while (new Date().getTime() < start + millis);
-	}
+	var content = "empty";
 
-	sleep(10000);
-	return "Hello Start";
+	exec("ls -lah", function (error, stdout, stderr) {
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(stdout);
+		response.end();
+	});
 }
 
-function upload () {
+function upload (response) {
 	console.log("Request handler 'upload' was called.");
-	return "Hello Upload";
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello Upload");
+	response.end();
 }
 
 exports.start = start;
--- node.js/server.js
+++ node.js/server.js
@@ -6,12 +6,7 @@ function start (route, handle) {
 	function onRequest (request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-
-		var content = route(handle, pathname);
-
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(content);
-		response.end();
+		route(handle, pathname, response);
 	}
 
 	http.createServer(onRequest).listen(8888);
