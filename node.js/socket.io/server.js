1ea5e43aef6d25c1ff2834985a5f7995b4f27018
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2012-12-26 22:36:45 -0200

    Versão 2 funcionando. Nesta versão tudo funciona através do servidor web. Isto é, não preciso acessar o ARQUIVO (file://), mas apenas usar http://localhost para funcionar. Veja os comentários nos arquivos, pois enfrentei bastante problemas até conseguir fazer funcionar.

--- node.js/socket.io/server.js
+++ node.js/socket.io/server.js
@@ -1,5 +1,22 @@
-// Server
-var io = require('socket.io').listen(80);
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+// ATENÇÃO, que sofri com isso: usuários comuns (não root) não podem usar porta menor que a 1024. Por isso, para rodar esse server é preciso fazê-lo usando sudo.
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(data);
+  });
+}
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
