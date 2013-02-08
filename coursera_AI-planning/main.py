b98016f64d8cd5baf088286ac29f8cdfdf03de48
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2013-02-07 22:26:45 -0200

    Removi arquivos Python e Java obsoletos.

--- coursera_AI-planning/main.py
+++
@@ -1,8 +0,0 @@
-from state import State
-from action import L2RAction, R2LAction
-
-s1 = State(3, 3, 1)
-a1 = L2RAction(1,1)
-s2 = s1.evolve(a1)
-a2 = R2LAction(1,0)
-s3 = s2.evolve(a2)
--- coursera_AI-planning/node.py
+++
@@ -1,6 +0,0 @@
-class Node ():
-	state = None
-	parent = None
-	action = None
-	cost = 0
-	depth = 0
