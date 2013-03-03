1db739a0f203c7eda7dda7133a9d68890be3af74
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2013-03-03 18:28:56 -0300

    Numerical exercises (week 2): FFT, truncating and padding

--- coursera/DSP/numerical_exercises_week2.m
+++ coursera/DSP/numerical_exercises_week2.m
@@ -1,43 +1,22 @@
 % ------------------------------
 % Input signal ('time' domain) composed of three tones
-N = 500;
-x1 = sin(2*pi* 40*(1:N)/N); % tone 1
-x2 = sin(2*pi* 80*(1:N)/N); % tone 2
-x3 = sin(2*pi*160*(1:N)/N); % tone 3
-x = [x1 zeros(1,N) x2 zeros(1,N) x3]';
-%wavplay(x);
-N = length(x);
+x1 = sin(2*pi* 40*(1:4000)/1000); % tone 1
+x2 = sin(2*pi* 50*(1:4000)/1000); % tone 2
+x = x1 + x2;
+
+N = length(x); % Full length of the signal
+M = 50;        % Number of samples for resampling (change to 4000 and compare)
 
 % ------------------------------
-% Calculates the Discrete Fourier Transform (DFT) transformation matrix
-a = 0:(N-1);
-A = repmat(a',1,N);
-B = repmat(a, N,1);
-
-% DFT transformation matrix. Notice that it is independent of x
-WN = exp(-i * (2*pi/N) * A .* B); 
+% Fast Fourier Transform (FFT)
+% Notice: with M = 50 it isn't possible to see the two peaks (tones).
+X = fft(x, M);         % FFT of the resampled signal: length M
+%plot((1:M)/M, abs(X));
 
 % ------------------------------
-% x-axis: t (time domain) or k (frequency domain)
-t = (1:N);
-k = t ./ N; % Normalized frequencies: (0,1]
-
-% ------------------------------
-% DFT of x
-%X = WN * x;
-%plot(k, abs(X));
-
-% ------------------------------
-% DFT in a reduced base:
-% WNreduced is a cut-off of the original DFT transformation matrix. That
-% means it represents a low-dimension DFT. As a result, the three tones
-% (peaks) where merged into two, a process known as "frequency mixing".
-WNreduced = [WN(1:600,:); WN(N-600:N,:)];
-inverseWNreduced = conj([WN(:,1:600) WN(:,N-600:N)]);
-Xapprox = WNreduced * x;
-xapprox = 1/N * real(inverseWNreduced * Xapprox);
-
-X = WN * xapprox;
-%plot(k, abs(X));
-%wavplay(xapprox);
-
+% Inverse FFT
+% Notice: increase the signal length here has no effect on distinguish the two peaks.
+% That is, padding the signal with zeros does not recover any information, as expected.
+x = ifft(X);           % x now has length M
+X = fft(x, N);         % Because N > M, pad M-lneght signal with zeros up to N samples
+plot((1:N)/N, abs(X));
