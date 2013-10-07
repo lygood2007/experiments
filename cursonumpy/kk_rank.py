dcfcf1caa14bc45dc5b80f3380d5b1f24efaa14e
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2013-10-06 23:16:02 -0300

    dtype='uint8'. Melhorou um pouco o tempo de execução.

--- cursonumpy/kk_rank.py
+++ cursonumpy/kk_rank.py
@@ -10,14 +10,14 @@ def kk_rank(f, opt):
     
     H,W = f.shape
     
-    layers = np.zeros((9,H+2,W+2))
+    layers = np.empty((9,H+2,W+2), dtype="uint8")
     c = 0
     for i in range(0,3):
         for j in range(0,3):
             layers[c,i:H+i,j:W+j] = f
             c += 1
     
-    ans = np.empty((H+2,W+2))        
+    ans = np.empty((H+2,W+2), dtype="uint8")
     ans[1,   1  ] = np.round(function(layers[[0,1,  3,4        ]], axis=0))[1,   1  ] # top-left-hand corner
     ans[2:H, 1  ] = np.round(function(layers[[0,1,  3,4,  6,7  ]], axis=0))[2:H, 1  ] # left line
     ans[  H, 1  ] = np.round(function(layers[[      3,4,  6,7  ]], axis=0))[  H, 1  ] # bottom-left-hand corner
@@ -29,6 +29,7 @@ def kk_rank(f, opt):
     ans[2:H, 2:W] = np.round(function(layers[[0,1,2,3,4,5,6,7,8]], axis=0))[2:H, 2:W] # middle
         
     return ans[1:H+1,1:W+1]
+
 
 f = np.random.rand(3,3)
 out = kk_rank(f, "max")
