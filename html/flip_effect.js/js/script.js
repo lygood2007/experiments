d260e11315abe4990bb6c9c4706dbaaf21f1d1ce
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2013-10-08 19:24:11 -0300

    Objeto card define os estados FRONT, BACK, FLIPPING e a flag flipped por conta própria.

--- html/flip_effect.js/js/script.js
+++ html/flip_effect.js/js/script.js
@@ -14,57 +14,66 @@
 
 	var Card = {
 		
-		x0: 0,
-		w: 0,
-		w0: 72,
-		view: null,
-		prev_cosa: 0,
-		angle: 0,
-		face: false,
+		left0: 0,
 		left: 0,
+		width0: 72,
+		width: 0,		
+		view: null,
+		//prev_cosa: 0,
+		angle: 0,
+		
+		EPS: 1e-5,
+		x: 80,
+		face: true,
+		state: "FRONT", // FRONT, FLIPPING, BACK
 
 		set_view: function(view) {
 			this.view = view;
-			this.x0 = 0;//view.left;
-			this.w0 = view.width();
+			this.left0 = 0;//view.left;
+			this.width0 = view.width();
 		},
 
-		evolve: function (angle) {
-			//if (this.view === null) throw new Error("Card.view must be set");
+		rotate: function (angle) {
+
+			// Need a view to flip
+			if (this.view === null) throw new Error("Card.view must be set");
 			
+			// Angle of rotation must lie in [0,π]
+			if (angle < 0 || angle > Math.PI) throw new Error("Invalid range: angle must lie in [0,π].")
 
-			var left = this.get_x(angle);
-			var width = this.get_w(angle);			
-			var scale = width / this.w0;
-			var bgw = scale * 512;
-			var pos = this.face ? -80 * scale : 0;
+			// State
+			if (angle < this.EPS) this.state = "FRONT";
+			else if (Math.PI - angle < this.EPS) this.state = "BACK";
+			else this.state = "FLIPPING";
 
+			// Whether the card face is up (true) or down
+			this.face = (angle < Math.PI / 2);
+
+			// this.left
+			var cosa = Math.cos(angle);
+			var s = cosa > 0 ? -1 : +1;
+			this.left = Math.round(this.left0 + this.width0 * (1 + s * cosa) / 2);
+
+			// this.width
+			var cosa = Math.cos(angle);
+			this.width = Math.round(Math.abs(this.width0 * cosa));
+		
+			// Scale is needed to reposition mask on the spritesheet
+			var scale = this.width / this.width0;
+			var background_scaled_width = Math.round(scale * 512);
+			var spritesheet_scaled_x = this.face ? Math.round(-this.x * scale) : 0;
+
+			// Update DOM
 			this.view.css({
-				"left": left + "px",
-				"width": width + "px",
-				"background-size": Math.round(bgw) + "px 1024px",
-				"background-position": Math.round(pos) + "px 0px"
+				"margin-left": this.left + "px",
+				"width": this.width + "px",
+				"background-size": background_scaled_width + "px 1024px",
+				"background-position": spritesheet_scaled_x + "px 0px"
 			});
-
-			var cosa = Math.cos(angle);
-			if (cosa * this.prev_cosa < 0 ) this.face = !this.face;
-			this.prev_cosa = cosa;
 		},
-
-		get_x: function (angle) {
-			var cosa = Math.cos(angle);
-			var s = cosa > 0 ? -1 : +1;			
-			this.left = Math.round(this.x0 + this.w0 * (1 + s * cosa) / 2);
-			return this.left
-		},
-
-		get_w: function (angle) {
-			var cosa = Math.cos(angle);
-			this.w = Math.round(Math.abs(this.w0 * cosa));
-			return this.w;
-		}
-
 	};
+
+
 
 
 	var card = Object.create(Card);
@@ -75,31 +84,39 @@
 
 	var t = 0; // s
 	var dt = 0.01; // s
-	var omega = 2 * Math.PI / 0.5;
+	var omega = 2 * Math.PI / 0.3;
 	var count = 0;
 	var angle = 0;
 	var goon = function () {
-		//if (count == 15) return;
-		t += dt; // TODO: timemillis
+		//if (count == 5) return;
+
+		t += dt;
 		angle = omega * t;
-		
-		if (angle < Math.PI) {			
-			card.evolve(angle);
-			card2.evolve(angle);
+
+		if (angle < Math.PI && angle > 0) {			
+			card.rotate(angle);
 			setTimeout(goon, 1000 * dt);
 			
 		}
 		else {
-			//card.angle = Math.PI;
-			card.evolve(angle);
-			card2.evolve(angle);
+			if (!card.face) angle = Math.PI;
+			else angle = 0;
+
+			card.rotate(angle);
 		}
 
 		count += 1;
 	}
 
-	$("#1").click(function () {		
-		setTimeout(goon, 1000 * dt);
+	$("#1").click(function () {
+
+		if (card.state != "FLIPPING") {
+
+			if (card.state == "BACK") dt = -0.01;
+			else dt = 0.01;
+
+			setTimeout(goon, 1000 * dt);
+		}
 	});
 
 	
