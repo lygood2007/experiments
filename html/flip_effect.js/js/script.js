86d1013d411cdba2345ed7764520a32318d6398c
Ivan Ramos Pagnossin <irpagnossin.edu@gmail.com>
2013-10-08 15:53:54 -0300

    Refatoração

--- html/flip_effect.js/js/script.js
+++ html/flip_effect.js/js/script.js
@@ -13,96 +13,92 @@
 	};
 
 	var Card = {
-		dt: 0.1,
-		flipping: false,
-		front: null,
-		back: null,
-		x: 0,
+		
 		x0: 0,
-		w: 72,
+		w: 0,
 		w0: 72,
-		T: 0.5,
 		view: null,
 		prev_cosa: 0,
-		cont: true,
 		angle: 0,
 		face: false,
-		evolve: function (t, omega) {
+		left: 0,
+
+		set_view: function(view) {
+			this.view = view;
+			this.x0 = 0;//view.left;
+			this.w0 = view.width();
+		},
+
+		evolve: function (angle) {
 			//if (this.view === null) throw new Error("Card.view must be set");
-			var target = this.view;
-
-			this.angle = omega * t;
-			var cosa = Math.cos(this.angle);
-
-
-
-
 			
-			if (cosa > 0) this.x = this.x0 + this.w0 * (1 - cosa) / 2;
-			else this.x = this.x0 + this.w0 * (1 + cosa) / 2;
-			this.w = Math.abs(this.w0 * cosa);
-			var scale = this.w / this.w0;
+
+			var left = this.get_x(angle);
+			var width = this.get_w(angle);			
+			var scale = width / this.w0;
 			var bgw = scale * 512;
-			target.css('left', Math.round(this.x)).css('width', Math.round(this.w));
-			
-			target.css("background-size", Math.round(bgw) + "px 1024px");
-			if (!this.face) pos = "0px 0px";
-			else pos = Math.round(-80 * scale) + "px 0px";
+			var pos = this.face ? -80 * scale : 0;
 
-			target.css("background-position", pos);
+			this.view.css({
+				"left": left + "px",
+				"width": width + "px",
+				"background-size": Math.round(bgw) + "px 1024px",
+				"background-position": Math.round(pos) + "px 0px"
+			});
 
-
+			var cosa = Math.cos(angle);
 			if (cosa * this.prev_cosa < 0 ) this.face = !this.face;
-
 			this.prev_cosa = cosa;
+		},
+
+		get_x: function (angle) {
+			var cosa = Math.cos(angle);
+			var s = cosa > 0 ? -1 : +1;			
+			this.left = Math.round(this.x0 + this.w0 * (1 + s * cosa) / 2);
+			return this.left
+		},
+
+		get_w: function (angle) {
+			var cosa = Math.cos(angle);
+			this.w = Math.round(Math.abs(this.w0 * cosa));
+			return this.w;
 		}
+
 	};
 
-	/*var flipper1 = Object.create(Flipper, {
-		dt: {
-			value: 42,
-			writable: false,
-			enumerable: true,
-			configurable: true
-		}
-	});*/
 
 	var card = Object.create(Card);
-	/*card.front = $("#front");
-	card.back = $("#back");
-	card.back.hide();*/
-	/*console.log("w0 = " + card.view.width);
-	card.w0 = card.view.width;*/
-	card.view = $("#1");
+	card.set_view($("#1"));
 	
-
 	var card2 = Object.create(Card);
-	card2.view = $("#2");
+	card2.set_view($("#2"));
 
 	var t = 0; // s
-	var dt = 0.1; // s
+	var dt = 0.01; // s
 	var omega = 2 * Math.PI / 0.5;
 	var count = 0;
+	var angle = 0;
 	var goon = function () {
 		//if (count == 15) return;
-		if (card.angle < Math.PI) {
-			t += dt; // TODO: timemillis
-			card.evolve(t, omega);
-			card2.evolve(t, omega);
+		t += dt; // TODO: timemillis
+		angle = omega * t;
+		
+		if (angle < Math.PI) {			
+			card.evolve(angle);
+			card2.evolve(angle);
 			setTimeout(goon, 1000 * dt);
 			
 		}
 		else {
 			//card.angle = Math.PI;
-			card.evolve(0.5, omega);
-			card2.evolve(0.5, omega);
+			card.evolve(angle);
+			card2.evolve(angle);
 		}
 
 		count += 1;
 	}
 
-	$("#1").click(function () {
-		console.log("asdfas")
+	$("#1").click(function () {		
 		setTimeout(goon, 1000 * dt);
 	});
 
